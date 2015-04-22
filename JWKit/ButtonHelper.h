252f65e679f84c901f75e3039bcde2f64e8fa1e3
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-04-21 22:39:33 -0300

    Created a new class, ButtonHelper. To avoid repeat code to create corners rounded in a button.

---
+++ JWKit/ButtonHelper.h
@@ -0,0 +1,16 @@
+//
+//  ButtonHelper.h
+//  JWKit
+//
+//  Created by Jhonathan Wyterlin on 21/04/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface ButtonHelper : NSObject
+
+-(void)roundCornerButton:(UIButton *)button;
+
+@end
---
+++ JWKit/ButtonHelper.m
@@ -0,0 +1,17 @@
+//
+//  ButtonHelper.m
+//  JWKit
+//
+//  Created by Jhonathan Wyterlin on 21/04/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "ButtonHelper.h"
+
+@implementation ButtonHelper
+
+-(void)roundCornerButton:(UIButton *)button {
+    button.layer.cornerRadius = 5.0;
+}
+
+@end
